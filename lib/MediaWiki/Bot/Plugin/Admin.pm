ce2792f34544c49571f6dc7360d3913a0ee6f53f
Mike.lifeguard <mike.lifeguard@gmail.com>
2010-05-21 23:54:37 +0000

    Add rollback() to Admin plugin

--- lib/MediaWiki/Bot/Plugin/Admin.pm
+++ lib/MediaWiki/Bot/Plugin/Admin.pm
@@ -1,10 +1,12 @@
 package MediaWiki::Bot::Plugin::Admin;
+use strict;
+use warnings;
+#use diagnostics;
 use locale;
 use POSIX qw(locale_h);
 setlocale(LC_ALL, "en_US.UTF-8");
-use strict;
 
-our $VERSION = '0.2.1';
+our $VERSION = '0.0.1';
 
 =head1 NAME
 
@@ -36,21 +38,48 @@ Calling import from any module will, quite simply, transfer these subroutines in
 =cut
 
 sub import {
-	no strict 'refs';
-	foreach my $method (qw/sample_admin/) {
-		*{caller() . "::$method"} = \&{$method};
-	}
+    no strict 'refs';
+    my @methods = qw(rollback);
+    foreach my $method (@methods) {
+        *{caller() . "::$method"} = \&{$method};
+    }
 }
 
-=item sample_admin($arg)
 
+=item rollback($pagename, $username, $summary, $markbot)
+
+Uses rollback to revert to the last revision of $pagename not edited by the latest editor of that page. If $username is not the last editor of $pagename, you will get an error, however it is a good idea to set this. If you do not, the latest edit(s) will be rolled back, and you could end up rolling back something you didn't intend to. Therefore, $username should be considered required. The remaining parameters are optional: $summary (to set a custom rollback edit summary), and $markbot (which marks both the rollback and the edits that were rolled back as bot edits).
+
+    $bot->rollback("Linux", "Some Vandal");
+    # OR
+    $bot->rollback("Wikibooks:Sandbox", "Mike.lifeguard", "test", 1);
 
 =cut
 
-sub sample_admin {
-	my $self    = shift;
+sub rollback {
+    my $self      = shift;
+    my $page      = shift;
+    my $user      = shift;
+    my $summary   = shift;
+    my $markbot   = shift;
 
-	return;
+    my $hash = {
+        action  => 'rollback',
+        title   => $page,
+        user    => $user,
+        summary => $summary,
+        markbot => $markbot,
+    };
+    my $res = $self->{api}->edit($hash);
+    if (!$res) {
+        carp 'Error code: ' . $self->{api}->{error}->{code};
+        carp $self->{api}->{error}->{details};
+        $self->{error} = $self->{api}->{error};
+        return $self->{error}->{code};
+    }
+    else {
+        return $res;
+    }
 }
 
 1;
